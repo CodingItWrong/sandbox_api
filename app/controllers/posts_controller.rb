2f6305e3675f12642d143cfb594fce29cdd5b7bf
Josh Justice <me@codingitwrong.com>
2018-07-20 14:57:07 -0500

    Add frozen_string_literal

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PostsController < ApiController
   before_action :doorkeeper_authorize!
 end
--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PostResource < JSONAPI::Resource
   attributes :title, :body
 
