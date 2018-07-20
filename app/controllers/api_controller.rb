19cc07225b82175c34c2512d7c44eed7f03fde51
Josh Justice <me@codingitwrong.com>
2018-07-20 14:50:33 -0500

    Restrict users to their own posts

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,15 @@
 class ApiController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
+
+  private
+
+  def context
+    { current_user: current_user }
+  end
+
+  def current_user
+    if doorkeeper_token
+      @current_user = User.find(doorkeeper_token.resource_owner_id)
+    end
+  end
 end
--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -1,3 +1,31 @@
 class PostResource < JSONAPI::Resource
   attributes :title, :body
+
+  before_create do
+    _model.user = current_user
+  end
+
+  def self.records(options = {})
+    user = current_user(options)
+    user.posts
+  end
+
+  def self.creatable_fields(context)
+    super - [:user]
+  end
+
+  def self.updatable_fields(context)
+    super - [:user]
+  end
+
+  private
+
+  def self.current_user(options)
+    options.fetch(:context).fetch(:current_user)
+  end
+
+  def current_user
+    context.fetch(:current_user)
+  end
+
 end
